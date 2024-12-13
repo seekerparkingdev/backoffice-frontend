93714467f2cc2e29a71c8379a35a0ab1cd4e3815
Tomasdmiguel <tomasdeskate@hotmail.es>
2024-12-13 09:23:57 -0300

    Terminando la estructura de estacionamiento

--- src/components/estacionamiento/form/GeneralEdit.jsx
+++ src/components/estacionamiento/form/GeneralEdit.jsx
@@ -2,77 +2,157 @@ import React, { useState } from "react";
 import MapComponent from "../../venues/InfoGeneral/MapComponent";
 
 const GeneralEdit = () => {
-  const [position, setPosition] = useState([-34.6037, -58.3816]);
-  const [venue, setVenue] = useState({
-    nombre: "",
-    capacidad: "",
-    direccion: "",
-    dominio: "",
-    buscar: "deshabilitada",
+  const [datosFormulario, setDatosFormulario] = useState({
+    nombre: "Garage JorMar Thames",
+    propietario: "Mariano Silva",
+    email: "mysilva_garage@hotmail.com",
+    descripcion: "400 m",
+    codigoLugar: "THAMES539",
+    direccion: "Thames 539, Buenos Aires",
+    ubicacion: "https://maps.app.goo.gl/vu4GcAtgGxv9xeEjBA",
+    posicion: [-34.6037, -58.3816],
+    portada: null,
+    recomendar: false,
+    obligatorioLlaves: false,
+    incluirServiceCharge: false,
+    porcentajeServiceCharge: 12.0,
+    horarioEspecial: "00:00",
   });
 
-  const handleInputChange = (e) => {
-    const { name, value } = e.target;
-    setVenue({ ...venue, [name]: value });
+  const handleCambioInput = (e) => {
+    const { name, value, type, checked } = e.target;
+    setDatosFormulario({
+      ...datosFormulario,
+      [name]: type === "checkbox" ? checked : value,
+    });
+  };
+
+  const handleActualizarMapa = (posicion) => {
+    setDatosFormulario({ ...datosFormulario, posicion });
+  };
+
+  const handleArchivoSubido = (e) => {
+    const file = e.target.files[0];
+    setDatosFormulario({ ...datosFormulario, portada: file });
   };
 
   return (
-    <div className="p-4 bg-white shadow-lg rounded-bl-lg">
-      <form className="space-y-6">
-        <div className="grid grid-cols-1 sm:grid-cols-2 gap-4 items-end">
+    <div className="p-8 bg-white shadow-lg rounded-lg max-w-4xl mx-auto">
+      <form className="space-y-8">
+        {/* Información básica */}
+        <div className="grid grid-cols-1 sm:grid-cols-2 gap-6">
           <div>
-            <label htmlFor="nombre" className="block text-gray-700 font-medium">
+            <label
+              htmlFor="nombre"
+              className="block text-gray-700 font-medium mb-2"
+            >
               Nombre:
             </label>
             <input
               type="text"
               id="nombre"
               name="nombre"
-              value={venue.nombre}
-              onChange={handleInputChange}
-              className="w-full px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
+              value={datosFormulario.nombre}
+              onChange={handleCambioInput}
+              className="w-full px-4 py-3 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
             />
           </div>
 
           <div>
             <label
-              htmlFor="capacidad"
-              className="block text-gray-700 font-medium"
+              htmlFor="propietario"
+              className="block text-gray-700 font-medium mb-2"
             >
-              Capacidad Máxima:
+              Propietario:
+            </label>
+            <select
+              name="propietario"
+              value={datosFormulario.propietario}
+              onChange={handleCambioInput}
+              className="w-full px-4 py-3 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
+            >
+              <option value="Mariano Silva">Mariano Silva</option>
+              {/* Agrega más opciones de propietario según sea necesario */}
+            </select>
+          </div>
+        </div>
+
+        {/* Información de contacto */}
+        <div className="grid grid-cols-1 sm:grid-cols-2 gap-6">
+          <div>
+            <label
+              htmlFor="email"
+              className="block text-gray-700 font-medium mb-2"
+            >
+              Email para Notificaciones:
             </label>
             <input
-              type="number"
-              id="capacidad"
-              name="capacidad"
-              value={venue.capacidad}
-              onChange={handleInputChange}
-              className="w-full px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
+              type="email"
+              id="email"
+              name="email"
+              value={datosFormulario.email}
+              onChange={handleCambioInput}
+              className="w-full px-4 py-3 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
+            />
+          </div>
+
+          <div>
+            <label
+              htmlFor="descripcion"
+              className="block text-gray-700 font-medium mb-2"
+            >
+              Texto descriptivo:
+            </label>
+            <input
+              type="text"
+              id="descripcion"
+              name="descripcion"
+              value={datosFormulario.descripcion}
+              onChange={handleCambioInput}
+              className="w-full px-4 py-3 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
             />
           </div>
         </div>
 
-        <div className="grid grid-cols-1 sm:grid-cols-2 gap-4">
-          <div className="col-span-2">
+        {/* Información de ubicación */}
+        <div className="grid grid-cols-1 sm:grid-cols-2 gap-6">
+          <div>
+            <label
+              htmlFor="codigoLugar"
+              className="block text-gray-700 font-medium mb-2"
+            >
+              Código para generación de plazas:
+            </label>
+            <input
+              type="text"
+              id="codigoLugar"
+              name="codigoLugar"
+              value={datosFormulario.codigoLugar}
+              onChange={handleCambioInput}
+              className="w-full px-4 py-3 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
+            />
+          </div>
+
+          <div>
             <label
               htmlFor="direccion"
-              className="block text-gray-700 font-medium"
+              className="block text-gray-700 font-medium mb-2"
             >
-              Dirección:
+              Dirección (Ej: Humboldt 450, Villa Crespo):
             </label>
-            <div className="flex">
+            <div className="flex items-center space-x-3">
               <input
                 type="text"
                 id="direccion"
                 name="direccion"
-                value={venue.direccion}
-                onChange={handleInputChange}
-                className="flex-grow px-4 py-2 border border-gray-300 rounded-l-lg focus:outline-none focus:ring-2 focus:ring-blue-500 mr-3"
+                value={datosFormulario.direccion}
+                onChange={handleCambioInput}
+                className="flex-grow px-4 py-3 border border-gray-300 rounded-l-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
               />
               <button
                 type="button"
-                onClick={() => alert("Mapa actualizado")}
-                className="bg-blue-500 text-white py-2 px-4 rounded-r-lg hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500"
+                onClick={() => handleActualizarMapa(datosFormulario.posicion)}
+                className="bg-blue-500 text-white py-3 px-6 rounded-r-lg hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500"
               >
                 Actualizar Mapa
               </button>
@@ -80,49 +160,151 @@ const GeneralEdit = () => {
           </div>
         </div>
 
-        <div className="grid grid-cols-1 sm:grid-cols-2 gap-4">
-          <div>
-            <label
-              htmlFor="dominio"
-              className="block text-gray-700 font-medium"
-            >
-              Dominio (No incluir protocolo https/http):
-            </label>
-            <input
-              type="text"
-              id="dominio"
-              name="dominio"
-              value={venue.dominio}
-              onChange={handleInputChange}
-              className="w-full px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
-            />
-          </div>
-
-          <div>
-            <label htmlFor="buscar" className="block text-gray-700 font-medium">
-              Barra de búsqueda en home:
-            </label>
-            <select
-              name="buscar"
-              value={venue.buscar}
-              onChange={handleInputChange}
-              id="buscar"
-              className="w-full px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
-            >
-              <option value="deshabilitada">Deshabilitada</option>
-              <option value="habilitada">Habilitada</option>
-            </select>
-          </div>
+        {/* URL de Ubicación */}
+        <div>
+          <label
+            htmlFor="ubicacion"
+            className="block text-gray-700 font-medium mb-2"
+          >
+            URL de Ubicación:
+          </label>
+          <input
+            type="text"
+            id="ubicacion"
+            name="ubicacion"
+            value={datosFormulario.ubicacion}
+            onChange={handleCambioInput}
+            className="w-full px-4 py-3 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
+          />
         </div>
 
+        {/* Foto de portada */}
+        <div>
+          <label
+            htmlFor="portada"
+            className="block text-gray-700 font-medium mb-2"
+          >
+            Foto de Portada:
+          </label>
+          <input
+            type="file"
+            id="portada"
+            name="portada"
+            accept="image/*"
+            onChange={handleArchivoSubido}
+            className="w-full px-4 py-3 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
+          />
+        </div>
+
+        {/* Mapa */}
         <div className="mt-6">
           <label className="block text-gray-700 font-medium mb-2">
             Seleccione la posición donde se encuentre el Venue o actualice el
             mapa mediante la dirección por coordenadas.
           </label>
           <div className="mt-6">
-            <MapComponent position={position} setPosition={setPosition} />
+            <MapComponent
+              position={datosFormulario.posicion}
+              onUpdate={handleActualizarMapa}
+            />
           </div>
+        </div>
+
+        {/* Opciones relacionadas con el lugar */}
+        <div className="space-y-6 mt-8">
+          <div className="flex items-center space-x-6">
+            <div className="flex items-center">
+              <input
+                type="checkbox"
+                id="recomendar"
+                name="recomendar"
+                checked={datosFormulario.recomendar}
+                onChange={handleCambioInput}
+                className="h-5 w-5 text-blue-500"
+              />
+              <label htmlFor="recomendar" className="text-gray-700 font-medium">
+                Recomendar
+              </label>
+            </div>
+
+            <div className="flex items-center">
+              <input
+                type="checkbox"
+                id="obligatorioLlaves"
+                name="obligatorioLlaves"
+                checked={datosFormulario.obligatorioLlaves}
+                onChange={handleCambioInput}
+                className="h-5 w-5 text-blue-500"
+              />
+              <label
+                htmlFor="obligatorioLlaves"
+                className="text-gray-700 font-medium"
+              >
+                Obligatorio dejar llaves
+              </label>
+            </div>
+
+            <div className="flex items-center">
+              <input
+                type="checkbox"
+                id="incluirServiceCharge"
+                name="incluirServiceCharge"
+                checked={datosFormulario.incluirServiceCharge}
+                onChange={handleCambioInput}
+                className="h-5 w-5 text-blue-500"
+              />
+              <label
+                htmlFor="incluirServiceCharge"
+                className="text-gray-700 font-medium"
+              >
+                Incluir Service Charge dentro del precio
+              </label>
+            </div>
+          </div>
+
+          <div>
+            <label
+              htmlFor="porcentajeServiceCharge"
+              className="block text-gray-700 font-medium mb-2"
+            >
+              Service Charge (%):
+            </label>
+            <input
+              type="number"
+              id="porcentajeServiceCharge"
+              name="porcentajeServiceCharge"
+              value={datosFormulario.porcentajeServiceCharge}
+              onChange={handleCambioInput}
+              className="w-full px-4 py-3 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
+            />
+          </div>
+
+          <div>
+            <label
+              htmlFor="horarioEspecial"
+              className="block text-gray-700 font-medium mb-2"
+            >
+              Horario Especial de Salida:
+            </label>
+            <input
+              type="time"
+              id="horarioEspecial"
+              name="horarioEspecial"
+              value={datosFormulario.horarioEspecial}
+              onChange={handleCambioInput}
+              className="w-full px-4 py-3 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
+            />
+          </div>
+        </div>
+
+        {/* Botón para guardar */}
+        <div className="mt-8 text-center">
+          <button
+            type="submit"
+            className="bg-green-500 text-white py-3 px-6 rounded-lg hover:bg-green-600 focus:outline-none focus:ring-2 focus:ring-green-500"
+          >
+            Guardar Cambios
+          </button>
         </div>
       </form>
     </div>
--- src/page/estacionamiento/EstacionamientoEditPage.jsx
+++ src/page/estacionamiento/EstacionamientoEditPage.jsx
@@ -2,44 +2,81 @@ import { useState } from "react";
 import { TituloParrafo } from "../../components/TituloParrafo";
 import { GeneralEdit } from "../../components/estacionamiento/form/GeneralEdit";
 import { BiSolidArrowToBottom, BiSolidArrowToTop } from "react-icons/bi";
-const EstacionamientoEditPage = () => {
-    
-      const [visibility, setVisibility] = useState({
-        generalEdit: false,
-        redes: false,
-        recomendado: false,
-        VenueEventos: false,
-      });
-    
-      const handleToggleVisibility = (section) => {
-        setVisibility((prevState) => ({
-          ...prevState,
-          [section]: !prevState[section],
-        }));
-      };
-    return (
-        <div>
-                  <div>
-          <div className="bg-white shadow-md rounded-t-lg p-6 flex items-center justify-between">
-            <TituloParrafo
-              titulo="Información General"
-              parrafo="Nombre, capacidad máxima, dirección y fotos"
-            />
-            <button
-              onClick={() => handleToggleVisibility("generalEdit")}
-              className="p-2 rounded hover:bg-gray-200"
-            >
-              {visibility.InfoGeneral ? (
-                <BiSolidArrowToTop size={20} className="text-gray-600" />
-              ) : (
-                <BiSolidArrowToBottom size={20} className="text-gray-600" />
-              )}
-            </button>
-          </div>
-          {visibility.generalEdit && <GeneralEdit  />}
-        </div>
-        </div>
-    )
-}
+import { PlazasEstacionamiento } from "../../components/estacionamiento/grilla/PlazasEstacionamiento";
 
-export {EstacionamientoEditPage}
+const EstacionamientoEditPage = () => {
+  const [visibility, setVisibility] = useState({
+    generalEdit: false,
+    precios: false,
+  });
+
+  const handleToggleVisibility = (section) => {
+    setVisibility((prevState) => ({
+      ...prevState,
+      [section]: !prevState[section],
+    }));
+  };
+
+  return (
+    <div className="max-w-screen-lg mx-auto px-4 sm:px-6 lg:px-8 mt-36">
+      <div>
+        <div className="bg-white shadow-md rounded-t-lg p-4 sm:p-6 flex items-center justify-between flex-wrap gap-4">
+          <TituloParrafo
+            titulo="Información General"
+            parrafo="Nombre, capacidad máxima, dirección y fotos"
+          />
+          <button
+            onClick={() => handleToggleVisibility("generalEdit")}
+            className="p-2 rounded hover:bg-gray-200 transition duration-150"
+          >
+            {visibility.generalEdit ? (
+              <BiSolidArrowToTop size={20} className="text-gray-600" />
+            ) : (
+              <BiSolidArrowToBottom size={20} className="text-gray-600" />
+            )}
+          </button>
+        </div>
+
+        {visibility.generalEdit && (
+          <div className="bg-gray-50 p-4 sm:p-6 rounded-b-lg shadow-inner">
+            <GeneralEdit />
+          </div>
+        )}
+      </div>
+
+      <div>
+        <div className="bg-white shadow-md rounded-t-lg p-4 sm:p-6 flex items-center justify-between flex-wrap gap-4 mt-32">
+          <div className="flex flex-col">
+            <h1 className="text-lg font-bold text-gray-800">
+              Precios de las Plazas
+            </h1>
+            <p className="text-sm text-gray-500">
+              Añada los precios para los diferentes tipos de plaza.
+              <br /> Selecciones los vehículos que acepta el estacionamiento y
+              en que orden desea que aparezcan.
+            </p>
+          </div>
+
+          <button
+            onClick={() => handleToggleVisibility("precios")}
+            className="p-2 rounded hover:bg-gray-200 transition duration-150"
+          >
+            {visibility.precios ? (
+              <BiSolidArrowToTop size={20} className="text-gray-600" />
+            ) : (
+              <BiSolidArrowToBottom size={20} className="text-gray-600" />
+            )}
+          </button>
+        </div>
+
+        {visibility.precios && (
+          <div className="bg-gray-50 p-4 sm:p-6 rounded-b-lg shadow-inner">
+            <PlazasEstacionamiento />
+          </div>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export { EstacionamientoEditPage };
